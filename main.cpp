e75b66088fba045a659590016ef4419aae315db3
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-11-20 16:35:46 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
+int main() 
+{
+  int i, j, multiplo;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+  for (i = 1; i <= 10; i++) 
+  {
+    cout << i << ": ";
+    for (j = 1; j <= 10; j++) 
+    {
+      multiplo = i * j;
+      cout << multiplo << " ";
+    }
+    cout << "" << endl;
+  }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
